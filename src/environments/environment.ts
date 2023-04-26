6d1c2185a38fac3697ed5102935fc3f3aa1938a3
Lennon <lenonart14@gmail.com>
2023-04-25 23:15:14 -0400

    first commit - deps installed and configured

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'nirby-pokemon-test',
+    appId: '1:610071602194:web:39622ca1986fae239e24cb',
+    storageBucket: 'nirby-pokemon-test.appspot.com',
+    apiKey: 'AIzaSyBAVXQBR51FnO-UZTgG2Svq3LDA-25Wl3w',
+    authDomain: 'nirby-pokemon-test.firebaseapp.com',
+    messagingSenderId: '610071602194',
+  },
   production: false
 };
 
