a9d785d5fca367000f837181684ec72bb0d6eb6f
Lennon <lenonart14@gmail.com>
2023-04-27 13:02:57 -0400

    pokemon interface changes

--- src/app/interfaces/Pokemon.interface.ts
+++ src/app/interfaces/Pokemon.interface.ts
@@ -1,9 +1,6 @@
 export default interface PokemonData {
   id: string;
-  number: string;
   name: string;
-  image: string;
-  fetchedAt: string;
   types: Array<{
     slot: number;
     type: {
@@ -12,14 +9,14 @@ export default interface PokemonData {
     };
   }>;
   sprites: {
-    back_default: string | null;
-    back_female: string | null;
-    back_shiny: string | null;
-    back_shiny_female: string | null;
     front_default: string | null;
-    front_female: string | null;
-    front_shiny: string | null;
-    front_shiny_female: string | null;
+    back_default?: string | null;
+    back_female?: string | null;
+    back_shiny?: string | null;
+    back_shiny_female?: string | null;
+    front_female?: string | null;
+    front_shiny?: string | null;
+    front_shiny_female?: string | null;
   };
 }
 
--- src/app/utils/formatDate.ts
+++ src/app/utils/formatDate.ts
@@ -2,7 +2,7 @@ import { DatePipe } from '@angular/common';
 
 const formatDate = (date: Date | string): string | null => {
   const datePipe: DatePipe = new DatePipe('en');
-  const parsedDate = datePipe.transform(date, 'dd/mm/yyyy hh:mm');
+  const parsedDate = datePipe.transform(date, 'dd/MM/yyyy hh:mm');
   return parsedDate;
 };
 
